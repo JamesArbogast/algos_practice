971d40326d1b4356c98fc42862f82e29171aaf0d
James Arbogast <jamesarbogast52@gmail.com>
2021-10-11 14:09:02 -0700

    anagram refactored

--- Anagram.js
+++ Anagram.js
@@ -35,3 +35,30 @@ function anagram(str1, str2) {
 
 console.log(anagram("ooz", "zoo"));
 
+//refactored
+
+function validAnagram(first,second) {
+  if(first.length !== second.length) {
+    return false;
+  }
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    //if letter exists , increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  for(let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if(!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  
+  return true;
+}
+
+console.log(validAnagram('anagram', 'nagaram'));
+
